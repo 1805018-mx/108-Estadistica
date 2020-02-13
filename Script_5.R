078bc0115b24d1aa5ca82c0a40c12cc43b9491f7
Elian Izaguirre <elian_beto@outlook.com>
2020-02-13 09:20:23 -0600

    actualizar

--- Script_5.R
+++ Script_5.R
@@ -11,4 +11,57 @@ shapiro.test(conjunto$Altura)
 ks.test(conjunto$Diametro,"pnorm", mean= mean(conjunto$Diametro, sd=sd(conjunto$Diametro)))
 
 hist(conjunto$Diametro)
-        
+
+
+shapiro.test(conjunto$Diametro)      
+
+
+boxplot(conjunto$Diametro~ conjunto$Especie, col="grey")
+boxplot(conjunto$Vecinos~ conjunto$Especie)
+boxplot(conjunto$Clase~ conjunto$Especie)
+
+
+frecu <- table(conjunto$Clase)
+prop.table(frecu)
+hist(frecu)
+
+
+
+
+# Prueba Homogenidad ------------------------------------------------------
+
+var(conjunto$Diametro)
+var(conjunto$Altura)
+
+
+
+
+# Datos Ebanos ------------------------------------------------------------
+
+ebano<- read.csv("Clases/ebanos.csv", header =T)
+hist(ebano$diametro)
+hist(ebano$altura)
+
+boxplot(ebano)
+boxplot(ebano$diametro)
+boxplot(ebano$altura)
+
+
+
+fivenum(ebano$diametro)
+fivenum(ebano$altura)
+
+
+#¿Cuántos individuos tienen un Diam menor al 1er quartil?
+
+sum(ebano$diametro <= 15.55)
+
+#¿Cuántos individuos tienen un Diam menor al 3er quartil?
+
+sum(ebano$diametro <=34.25)
+
+#¿Cuántos individuos tienen un Diam mayor al 1er quartil
+#pero menor o igual a el 2do quartil?
+
+sum(ebano$diametro >=15.55 & ebano$diametro <= 25.90) 
+sum(ebano$diametro >=10 & ebano$diametro <=15.55)
