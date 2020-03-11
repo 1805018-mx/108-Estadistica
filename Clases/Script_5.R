cef14d9a9b7c0b9e773cb71105b33be1c325831e
Elian Izaguirre <elian_beto@outlook.com>
2020-03-11 08:26:17 -0600

    Pruebas T

---
+++ Clases/Script_5.R
@@ -0,0 +1,14 @@
+#Elian Roberto Izaguirre Garcia
+#1805018
+#11.03.2020
+
+#Importar datos vivero
+setwd("C:/Tareas1/108-Estadistica/Clases")
+
+
+# IMPORTAR DATOS ----------------------------------------------------------
+
+
+Vivero <- read.csv("viveros.csv", header = T)
+
+summary(Vivero)
